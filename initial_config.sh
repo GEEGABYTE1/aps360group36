98353e4b2dbe9dd1fe3ed6c83a8fe4a15d82ec44
Awesome_Ace22 <amelia.c.ellis@icloud.com>
2024-10-31 11:33:36 -0400

    changes to config scripts for rasberrypiOS updates

--- initial_config.sh
+++ initial_config.sh
@@ -1,25 +1,92 @@
 #!/bin/bash
 
 # Save a log of all commands and their output
-exec > /path/to/directory/session.log 2>&1
+
 
 # Update and upgrade the system
 echo "Updating system..."
 sudo apt update && sudo apt upgrade -y
 
 # Install required packages
+echo "Installing packages..."
 sudo apt install ufw
 sudo apt install -y dnsmasq docker-compose
 
+# Backup existing dhcpcd.conf and create a new one
+echo "Backing up Files..."
+sudo cp /etc/dhcpcd.conf /etc/dhcpcd.conf.v1
+
+# Install dhcpcd5
+echo "Installing packages..."
+sudo apt install dhcpcd5 --fix-missing
+sudo apt install --reinstall raspberrypi-ui-mods
+
 #firewall set up
+echo "Setting up firewall ..."
 sudo ufw allow 8000/tcp
 sudo ufw allow 8000/udp
-sudo ufw enable
+sudo ufw disable
 # sudo ufw status
+
+# Backup existing dhcpcd.conf,dnsmasq.conf, and host files
+echo "Backing up Files..."
+sudo mv /etc/dhcpcd.conf /etc/dhcpcd.conf.v2
+sudo cp /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
+sudo cp /etc/hosts /etc/hosts.orig
+
 
 # Set a static IP address for Ethernet (eth0)
 echo "Configuring static IP address for for the Raspberry Pi Ethernet..."
 sudo bash -c 'cat << EOF >> /etc/dhcpcd.conf
+# A sample configuration for dhcpcd.
+# See dhcpcd.conf(5) for details.
+
+# Allow users of this group to interact with dhcpcd via the control socket.
+#controlgroup wheel
+
+# Inform the DHCP server of our hostname for DDNS.
+#hostname
+
+# Use the hardware address of the interface for the Client ID.
+#clientid
+# or
+# Use the same DUID + IAID as set in DHCPv6 for DHCPv4 ClientID as per RFC4361.
+# Some non-RFC compliant DHCP servers do not reply with this set.
+# In this case, comment out duid and enable clientid above.
+duid
+
+# Persist interface configuration when dhcpcd exits.
+persistent
+
+# vendorclassid is set to blank to avoid sending the default of
+# dhcpcd-<version>:<os>:<machine>:<platform>
+vendorclassid
+
+# A list of options to request from the DHCP server.
+option domain_name_servers, domain_name, domain_search
+option classless_static_routes
+# Respect the network MTU. This is applied to DHCP routes.
+option interface_mtu
+
+# Request a hostname from the network
+option host_name
+
+# Most distributions have NTP support.
+#option ntp_servers
+
+# Rapid commit support.
+# Safe to enable by default because it requires the equivalent option set
+# on the server to actually work.
+option rapid_commit
+
+# A ServerID is required by RFC2131.
+require dhcp_server_identifier
+
+# Generate SLAAC address using the Hardware Address of the interface
+#slaac hwaddr
+# OR generate Stable Private IPv6 Addresses based from the DUID
+slaac private
+
 interface eth0
 static ip_address=192.168.0.1
 static routers=192.168.0.1
@@ -30,19 +97,29 @@ EOF'
 echo "Setting up dnsmasq for DHCP..."
 sudo bash -c 'cat << EOF > /etc/dnsmasq.conf
 interface=eth0
-listen-address=192.168.0.1
 bind-dynamic
 domain-needed
 bogus-priv
-dhcp-range=192.168.0.2,192.168.0.10,255.255.255.0,12h
+expand-hosts
+domain=local
+dhcp-range=192.168.0.2,192.168.0.150,255.255.255.0,12h
 EOF'
 
-# Restart the necessary services
-echo "Restarting services to apply changes..."
-sudo systemctl restart dhcpcd        # Restart DHCP client daemon
-sudo service dnsmasq restart     # Restart dnsmasq to apply changes
+#Add a local host to the hosts file
+echo "Adding local host..."
+sudo bash -c 'cat << EOF > /etc/hosts
+127.0.0.1	localhost
+::1		localhost ip6-localhost ip6-loopback
+ff02::1		ip6-allnodes
+ff02::2		ip6-allrouters
+
+127.0.1.1	raspberrypi
+192.168.0.1 novaOps
+EOF'
 
 # Check if Docker is installed
+echo "Installing Docker..."
+
 if command -v docker &> /dev/null
 then
     echo "Docker is already installed."
@@ -58,3 +135,4 @@ fi
 #reboot
 echo "Rebooting Raspberry Pi..."
 sudo reboot
+
--- post_reboot_config.sh
+++ post_reboot_config.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# manual eth0 IP  setting
+echo "Configuring IP address"
+sudo ifconfig eth0 192.168.0.1 netmask 255.255.255.0 # Check with ip addr show eth0
+
 # Ask if the user wants to enable the container to run at boot
 read -p "Would you like Docker to run at boot? (y/n): " run_at_boot
 
@@ -13,6 +17,22 @@ else
 fi
 
 # Build and run Docker container using docker-compose
-echo "Starting FastAPI container..."
-sudo docker-compose up --build
+echo "Building FastAPI container..."
+sudo docker-compose build
+sudo docker-compose up --no-start
 
+# Start DHCP service
+echo "Starting dhcpcd..."
+sudo systemctl enable dhcpcd
+sudo systemctl start dhcpcd
+
+# Restart the necessary services
+echo "Restarting services to apply changes..."
+# Restart DHCP client daemon 
+sudo systemctl restart dhcpcd  # Check with sudo systemctl status dhcpcd 
+# Restart dnsmasq to apply changes
+sudo service dnsmasq restart # Check with sudo systemctl status dnsmasq
+
+# Start the Docker container using docker-compose
+echo "Starting FastAPI container..."
+sudo docker-compose up
